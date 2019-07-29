478b4abc0a012516c616719427a26de0bb33b790
Ivan Filho <ivanslf@gmail.com>
2019-07-29 00:31:42 -0300

    fix: filter was case sensitive

--- src/util/filter.ts
+++ src/util/filter.ts
@@ -1,6 +1,14 @@
 import filter from 'ramda/es/filter'
 import propSatisfies from 'ramda/es/propSatisfies'
 import contains from 'ramda/es/contains'
+import toLower from 'ramda/es/toLower'
+import compose from 'ramda/es/compose'
+
+const lowerContains = (value: string) =>
+  compose(
+    contains(value),
+    toLower
+  )
 
 export const searchBy = <T>(prop: string) => (value: string) =>
-  filter<T>(propSatisfies<string, T>(contains(value), prop))
+  filter<T>(propSatisfies<string, T>(lowerContains(value), prop))
